a477d1b8585191e326fb8be551038759eb0e8cb8
Derin DeNio <derindenio@gmail.com>
2026-07-23 18:50:02 -0600

    First challenge completed

---
+++ ts/main.ts
@@ -0,0 +1,13 @@
+//TypeScript infurs here that 'myName' is a string/automatically figured out the data type / set the data type.
+//let myName = "Bob";
+//Can't reassign myName to a number Type, as typeScript infured that 'myname' is a string type
+//myName = 4;
+
+//Let's manually give 'myName' a string type:
+let myName: string = "Bob";
+
+//Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
+
+let numberOfWheels: number = 4;
+let isStudent: boolean = false;
