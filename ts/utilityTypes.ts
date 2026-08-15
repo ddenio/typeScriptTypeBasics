fe2ee8dd63adcdc305974b50497698cb98dd1f14
Derin DeNio <derindenio@gmail.com>
2026-08-15 16:27:37 -0600

    Updating User data using Object.assign

---
+++ ts/utilityTypes.ts
@@ -0,0 +1,28 @@
+//Utility types and Partials
+
+type User2 = {
+  id: number;
+  username: string;
+  role: "member" | "contributor" | "admin";
+};
+
+const users2: User2[] = [
+  { id: 1, username: "john_doe", role: "member" },
+  { id: 2, username: "jane_smith", role: "contributor" },
+  { id: 3, username: "alice_jones", role: "admin" },
+  { id: 4, username: "charlie_brown", role: "member" },
+];
+
+function updateUser(id: number, updates: any) {
+  //Find the user in the array by the id
+  const userDetail = users2.find((userObj) => userObj.id === id);
+  //Use Object.assign to update the found user in place.
+  Object.assign(userDetail, updates);
+  //Check MDN if you need hlep with using Object.assign
+}
+
+//Example updates:
+updateUser(1, { username: "new_john_doe" });
+updateUser(4, { role: "contributor" });
+
+console.log(users2);
